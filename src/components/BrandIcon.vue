720441e888819cf465ac2bc15f19df4b41575406
Kyle Rodgers <kyle.rits.rodgers@gmail.com>
2019-03-31 12:49:33 -0400

    Building out the Toolbox. Gathering icons

---
+++ src/components/BrandIcon.vue
@@ -0,0 +1,54 @@
+<template>
+    <figure class="brand">
+        <!-- List of svg components -->
+        <Github v-if="icon == 'github'"/>
+        <Webpack v-if="icon == 'webpack'"/>
+        <Vue v-if="icon == 'vue'"/>
+        <Sass v-if="icon == 'sass'"/>
+        <Laravel v-if="icon == 'laravel'"/>
+        <Craft v-if="icon == 'craft'"/>
+        <Wordpress v-if="icon == 'wordpress'"/>
+        <Heroku v-if="icon == 'heroku'"/>
+        <Netlify v-if="icon == 'netlify'"/>
+    </figure>
+</template>
+
+<script>
+import Github from '~/assets/svgs/tools/github.svg'
+import Webpack from '~/assets/svgs/tools/webpack.svg'
+import Vue from '~/assets/svgs/tools/vue.svg'
+import Sass from '~/assets/svgs/tools/sass.svg'
+import Laravel from '~/assets/svgs/tools/laravel.svg'
+import Craft from '~/assets/svgs/tools/craft.svg'
+import Wordpress from '~/assets/svgs/tools/wordpress.svg'
+import Heroku from '~/assets/svgs/tools/heroku.svg'
+import Netlify from '~/assets/svgs/tools/netlify.svg'
+
+export default {
+    props: ['icon'],
+    components: {
+        Github,
+        Webpack,
+        Vue,
+        Sass,
+        Laravel,
+        Craft,
+        Wordpress,
+        Heroku,
+        Netlify
+    },
+}
+</script>
+
+<style lang="scss">
+.brand {
+    display: flex;
+    align-items: center;
+    svg {
+        width: 30px;
+        height: 30px;
+    }
+}
+</style>
+
+
--- src/components/ToolBox.vue
+++ src/components/ToolBox.vue
@@ -3,13 +3,22 @@
         <div class="container">
             <div class="row">
                 <div class="col-lg-5 col-md-6">
-                    <div class="info-wrap">
+                    <div class="info-wrap mb-5">
                         <slot></slot>
                     </div>
                 </div>
                 <div class="col-lg-7 col-md-6">
                     <div class="tool-grid">
-
+                        <!-- Tool Loop -->
+                        <div 
+                            v-for="(tool, index) in tools"
+                            :key="`tool-${index}`"
+                            class="tool-wrap">
+                            <button class="tool-button circle-button" :aria-label="tool.title">
+                                <!-- tool icon -->
+                                <brand-icon :icon="tool.icon" />
+                            </button>
+                        </div>
                     </div>
                 </div>
             </div>
@@ -18,8 +27,63 @@
 </template>
 
 <script>
+import BrandIcon from '~/components/BrandIcon.vue'
+
 export default {
-    
+    components: {
+        BrandIcon
+    },
+    data() {
+        return {
+            tools: [
+                {
+                    title: 'Github',
+                    desc: 'Collaborative project workflow tool.',
+                    icon: 'github'
+                },
+                {
+                    title: 'Webpack',
+                    desc: 'Code running and build tool.',
+                    icon: 'webpack'
+                },
+                {
+                    title: 'Vue',
+                    desc: 'Powerful and eloquent javascript framework.',
+                    icon: 'vue'
+                },
+                {
+                    title: 'Sass',
+                    desc: 'CSS processing language.',
+                    icon: 'sass'
+                },
+                {
+                    title: 'Laravel',
+                    desc: 'PHP framework.',
+                    icon: 'laravel'
+                },
+                {
+                    title: 'Craft',
+                    desc: 'Incredibly scalable and flexible CMS.',
+                    icon: 'craft'
+                },
+                {
+                    title: 'Wordpress',
+                    desc: 'Popular PHP based CMS.',
+                    icon: 'wordpress'
+                },
+                {
+                    title: 'Heroku',
+                    desc: 'Popular PHP based CMS.',
+                    icon: 'heroku'
+                },
+                {
+                    title: 'Netlify',
+                    desc: 'Popular PHP based CMS.',
+                    icon: 'netlify'
+                }
+            ]
+        }
+    },
 }
 </script>
 
@@ -28,9 +92,25 @@ export default {
 
 .tool-box {
     .tool-grid {
+        display: grid;
+        grid-template-columns: repeat(3, 1fr);
+        .tool-wrap {
+            display: flex;
+            justify-content: center;
+            .tool-button {
+                margin-bottom: 2rem;
+            }
+        }
         @media (min-width: $md) {
-            display: grid;
             grid-template-columns: repeat(4, 1fr);
+            .tool-wrap {
+                &:nth-child(even) {
+                    transform: translateY(50%);
+                }
+                .tool-button {
+                    margin-bottom: 4rem;
+                }
+            }
             
         }
     }
